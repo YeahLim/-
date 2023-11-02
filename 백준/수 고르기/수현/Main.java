cf35712c682c360db2275d8413b174e29e7e1481
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-11-02 22:09:45 +0900

    Update Main.java

--- 백준/수 고르기/수현/Main.java
+++ 백준/수 고르기/수현/Main.java
@@ -24,24 +24,23 @@ public class Main {
 		int l=0,r=0;
 		long min_differ = 10_000_000_001L;
 
-    //오른쪽 포인터가 오른쪽 바깥으로 나갔을 때 까지
+    		//오른쪽 포인터가 오른쪽 바깥으로 나갔을 때 까지
 		while(r < arr.length){
 			long differ = arr[r] - arr[l];
 
-      //M보다 크거나 같고 현재 차이가 최소 차이가 작으면 최소값 최신화
+      			//M보다 크거나 같고 현재 차이가 최소 차이가 작으면 최소값 최신화
 			if(differ >= M && min_differ > differ ){
 				min_differ = differ;
 			}
-      //차이가 M보다 크면 l~r 사이에 더 작은 차이가 있을 수 있으므로 차이를 줄여보기 위해 왼쪽 포인터 증가
+     			 //차이가 M보다 크면 l~r 사이에 더 작은 차이가 있을 수 있으므로 차이를 줄여보기 위해 왼쪽 포인터 증가
 			if(differ > M){
 				l++;
 			}
-      //차이가 M보다 작거나 같으면 l~r사이에 더이상 작은 차이가 없으므로 차이를 늘리기 위해 오른쪽 포인터 증가
-      else {
+      			//차이가 M보다 작거나 같으면 l~r사이에 더이상 작은 차이가 없으므로 차이를 늘리기 위해 오른쪽 포인터 증가
+    			  else {
 				r++;
-		  }
+		  	}
 		}
-
 		System.out.println(min_differ);
 	}
 }
